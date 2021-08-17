44b8e79b27df1bfa09661fcafb46ef09a9a134c8
mds2805 <54571744+mds2805@users.noreply.github.com>
2021-08-17 07:53:12 +0530

    Delete Library-management directory

--- Library-management/Library-management/src/main/java/com/example/service/BookService.java
+++
@@ -1,6 +0,0 @@
-package com.example.service;
-
-public interface BookService {
-	void addBook(String b_id, String b_name,String Author);
-
-}
--- Library-management/Library-management/src/main/java/com/example/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package com.example.service;
-
-public interface UserService {
-	
-   void doAdd(String id,String name,String Address);
-}
